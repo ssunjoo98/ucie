d69b32cfdeba44c27afbeaa82a3b81459c8a796c
Roshan Parekh <35009954+roshanparekh@users.noreply.github.com>
2026-06-01 23:02:00 -0700

    updated test to use centralized reference lfsr model

--- scala/test/src/uciedigital/logphy/UcieLFSRTest.scala
+++ scala/test/src/uciedigital/logphy/UcieLFSRTest.scala
@@ -2,6 +2,7 @@ package edu.berkeley.cs.uciedigital.logphy
 
 import chisel3._
 import chisel3.simulator.scalatest.ChiselSim
+import edu.berkeley.cs.uciedigital.utils.ReferenceLFSR
 import org.scalatest.funspec.AnyFunSpec
 
 import scala.util.Random
@@ -26,44 +27,6 @@ class UcieLFSRTest extends AnyFunSpec with ChiselSim {
     BigInt(0x1BB807)
   )
 
-  private class ReferenceLFSR(initSeed: BigInt, poly: BigInt, width: Int) {
-    private val mask = (BigInt(1) << width) - 1
-    private var state = initSeed
-
-    def reset(): Unit = {
-      state = initSeed
-    }
-
-    def outputWord(numBits: Int): BigInt = {
-      var nextState = state
-      var word = BigInt(0)
-
-      for (bit <- 0 until numBits) {
-        val msb = (nextState >> (width - 1)) & 1
-        word |= msb << bit
-        nextState = advanceOneBit(nextState, msb)
-      }
-
-      word
-    }
-
-    def advance(numBits: Int): Unit = {
-      for (_ <- 0 until numBits) {
-        val msb = (state >> (width - 1)) & 1
-        state = advanceOneBit(state, msb)
-      }
-    }
-
-    private def advanceOneBit(currentState: BigInt, msb: BigInt): BigInt = {
-      val shifted = (currentState << 1) & mask
-      if (msb == 1) {
-        shifted ^ poly
-      } else {
-        shifted
-      }
-    }
-  }
-
   private def params = AfeParams(mbSerializerRatio = serializerRatio, mbLanes = lanes)
 
   private def laneReferenceModels(): Seq[ReferenceLFSR] =
@@ -80,7 +43,7 @@ class UcieLFSRTest extends AnyFunSpec with ChiselSim {
 
   private def expectOutputs(dut: UcieLFSR, refs: Seq[ReferenceLFSR], context: String): Unit = {
     for (lane <- 0 until lanes) {
-      val expected = refs(lane).outputWord(serializerRatio)
+      val expected = refs(lane).peekOutputWord(serializerRatio)
       val actual = dut.io.lfsrOutput(lane).peek().litValue
       assert(
         actual == expected,
@@ -112,7 +75,7 @@ class UcieLFSRTest extends AnyFunSpec with ChiselSim {
           }
           dut.clock.step()
 
-          refs.foreach(_.advance(serializerRatio))
+          refs.foreach(_.advanceState(serializerRatio))
           clearControls(dut)
           expectOutputs(dut, refs, s"deterministic step $step after increment")
         }
@@ -144,7 +107,7 @@ class UcieLFSRTest extends AnyFunSpec with ChiselSim {
             if (doReset) {
               ref.reset()
             } else if (doIncrement) {
-              ref.advance(serializerRatio)
+              ref.advanceState(serializerRatio)
             }
           }
 
