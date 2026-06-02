13e66ef56c967a9f50a076412e02930e18111526
Roshan Parekh <35009954+roshanparekh@users.noreply.github.com>
2026-06-01 23:00:14 -0700

    fix: cleaned up test

--- scala/test/src/uciedigital/logphy/ParallelGaloisLFSRTest.scala
+++ scala/test/src/uciedigital/logphy/ParallelGaloisLFSRTest.scala
@@ -1,231 +1,113 @@
 package edu.berkeley.cs.uciedigital.utils
 
 import chisel3._
-import chisel3.util._
 import chisel3.simulator.scalatest.ChiselSim
-
+import edu.berkeley.cs.uciedigital.utils.TestHelpers.debug
 import org.scalatest.funspec.AnyFunSpec
 
 class ParallelGaloisLFSRTest extends AnyFunSpec with ChiselSim {
-
-  class referenceLFSR(initSeed: Int, poly: Int, width: Int) {
-    /*
-      The polynomial arguments corresponds to the taps.
-        !!! Forgo the leading term bit in polynomial !!!
-        Ex: If polynomial is G(X) = X^3 + X^2 + 1
-            Polynomial in binary: 1101 (X^3, X^2, X, 1)
-            The value used for polynomial will be: 0x5, NOT 0xD
-     */
-    private val polynomial: Int = poly
-    private var state: Int = initSeed
-    private var seed: Int = initSeed
-    private val mask: Int = (1 << width) - 1
-
-    def getState(): Int = state
-    def getStateHex(): String = f"${state}%X"
-    def getStateBitStr(): String =
-      state.toBinaryString.reverse.padTo(width, '0').reverse
-
-    def reset(): Unit = {
-      state = seed
-    }
-
-    def reseed(newSeed: Int): Unit = {
-      state = newSeed
-      seed = newSeed
-    }
-
-    def getMsb(): Int = {
-      (state >> (width - 1)) & 1
-    }
-
-    def increment(): Int = {
-      // shifts out of msb
-      val msb = (state >> (width - 1)) & 1
-
-      state = (state << 1) & mask
-      if (msb == 1) {
-        state = state ^ polynomial
-      }
-      msb
-    }
-
-    def doCycleOutput(numCycles: Int): (Int, BigInt) = {
-      var bitStr = ""
-      for (i <- 0 until numCycles) {
-        val msb = increment()
-        bitStr += msb.toBinaryString
-      }
-      (state, BigInt(bitStr.reverse, 2))
-    }
-  }
-
   val seed = 0x1dbfbc
   val lfsrWidth = 23
   val dataWidth = 32
   val polynomial = 0x210125
+  val printDebug = false
+
+  def bitString(value: BigInt, width: Int): String =
+    value.toString(2).reverse.padTo(width, '0').reverse
+
+  def expectInitialState(dut: ParallelGaloisLFSR, refLfsr: ReferenceLFSR): Unit = {
+    debug(printDebug, "[TEST] State at initialization")
+    debug(printDebug, "[TEST] Reference State: 0x" + refLfsr.getStateHex())
+    debug(printDebug, "[TEST] DUT State:       0x" + dut.io.state.peek().litValue.toString(16).toUpperCase)
+
+    dut.io.state.expect(refLfsr.getState().U, "DUT initial state doesn't match the reference state")
+  }
+
+  def checkOutputAndIncrement(
+    dut: ParallelGaloisLFSR,
+    refLfsr: ReferenceLFSR,
+    step: Int
+  ): Unit = {
+    debug(printDebug, s"[TEST] ======== Step ${step + 1} ========")
+
+    val expectedOutput = refLfsr.peekOutputWord(dataWidth)
+    refLfsr.advanceState(dataWidth)
+    val expectedState = refLfsr.getState()
+    val dutOutput = dut.io.lfsrOutput.peek().litValue
+
+    debug(printDebug, s"[TEST] Reference output bit string: ${bitString(expectedOutput, dataWidth)}")
+    debug(printDebug, s"[TEST] DUT output bit string:       ${bitString(dutOutput, dataWidth)}")
+
+    dut.io.lfsrOutput.expect(expectedOutput.U, "DUT output doesn't match the reference output")
+
+    dut.io.increment.poke(true.B)
+    dut.clock.step()
+    dut.io.increment.poke(false.B)
+
+    debug(printDebug, s"[TEST] Reference state after incrementing: 0x${expectedState.toString(16).toUpperCase}")
+    debug(printDebug, s"[TEST] DUT state after incrementing:       0x${dut.io.state.peek().litValue.toString(16).toUpperCase}")
+
+    dut.io.state.expect(expectedState.U, "DUT state doesn't match the reference state")
+  }
 
   describe("Parallel Galois LFSR Instantiation Test") {
     it("Instantiated ParallelGaloisLFSR") {
-      simulate(new ParallelGaloisLFSR(seed, lfsrWidth, dataWidth, polynomial)) {
-        c =>
-          c.clock.step()
-          println("[TEST] Success")
+      simulate(new ParallelGaloisLFSR(seed, lfsrWidth, dataWidth, polynomial)) { dut =>
+        dut.io.state.expect(seed.U, "DUT initial state doesn't match the seed")
+        dut.clock.step()
+        debug(printDebug, "[TEST] Success")
       }
     }
   }
 
   describe("Parallel Galois LFSR Sanity Check Test") {
     it("Parallel Galois LFSR works") {
-      simulate(new ParallelGaloisLFSR(seed, lfsrWidth, dataWidth, polynomial)) {
-        c =>
+      simulate(new ParallelGaloisLFSR(seed, lfsrWidth, dataWidth, polynomial)) { dut =>
+        val refLfsr = new ReferenceLFSR(seed, polynomial, lfsrWidth)
+        val numIncrements = 5
 
-          val refLfsr = new referenceLFSR(seed, polynomial, lfsrWidth)
-          println("[TEST] State at initialization")
-          println("[TEST] Reference State: 0x" + refLfsr.getStateHex())
-          println(
-            "[TEST] DUT State:       0x" + c.io.state
-              .peek()
-              .litValue
-              .toString(16)
-              .toUpperCase
-          )
-          c.io.state.expect(refLfsr.getState())
+        expectInitialState(dut, refLfsr)
 
-          val numIncrements = 5
+        for (step <- 0 until numIncrements) {
+          checkOutputAndIncrement(dut, refLfsr, step)
+        }
 
-          for (i <- 0 until numIncrements) {
-            println(s"[TEST] ======== Step ${i + 1} ========")
-            var refBitStr = ""
-            for (i <- 0 until dataWidth) {
-              val msb = refLfsr.increment()
-              refBitStr = msb.toString + refBitStr
-            }
-            val dutOutputBitStr = c.io.lfsrOutput
-              .peek()
-              .litValue
-              .toString(2)
-              .reverse
-              .padTo(dataWidth, '0')
-              .reverse
-
-            println(s"[TEST] Reference output bit string: ${refBitStr}")
-            println(s"[TEST] DUT output bit string:       ${dutOutputBitStr}")
-            assert(
-              refBitStr == dutOutputBitStr,
-              "DUT output doesn't match the reference output"
-            )
-
-            c.io.increment.poke(true.B)
-            c.clock.step()
-            c.io.increment.poke(false.B)
-
-            val dutStateHexStr =
-              c.io.state.peek().litValue.toString(16).toUpperCase
-            println(
-              s"[TEST] Reference state after incrementing: 0x${refLfsr.getStateHex()}"
-            )
-            println(
-              s"[TEST] DUT state after incrementing:       0x${dutStateHexStr}"
-            )
-
-            assert(
-              refLfsr.getStateHex() == dutStateHexStr,
-              "DUT state doesn't match the reference state"
-            )
-          }
-          println("[TEST] Success")
+        debug(printDebug, "[TEST] Success")
       }
     }
   }
 
   describe("Parallel Galois LFSR Intermediate Reset Test") {
     it("Parallel Galois LFSR reset signal works") {
-      simulate(new ParallelGaloisLFSR(seed, lfsrWidth, dataWidth, polynomial)) {
-        c =>
+      simulate(new ParallelGaloisLFSR(seed, lfsrWidth, dataWidth, polynomial)) { dut =>
+        val refLfsr = new ReferenceLFSR(seed, polynomial, lfsrWidth)
+        val numIncrements = 5
+        val whenToReset = 4 // 1 based count
 
-          val refLfsr = new referenceLFSR(seed, polynomial, lfsrWidth)
-          println("[TEST] State at initialization")
-          println("[TEST] Reference State: 0x" + refLfsr.getStateHex())
-          println(
-            "[TEST] DUT State:       0x" + c.io.state
-              .peek()
-              .litValue
-              .toString(16)
-              .toUpperCase
-          )
-          c.io.state.expect(refLfsr.getState())
+        expectInitialState(dut, refLfsr)
 
-          val numIncrements = 5
-          val whenToReset = 4 // 1 based count
+        for (step <- 0 until numIncrements) {
+          if (step == (whenToReset - 1)) {
+            debug(printDebug, s"[TEST] ======== Step ${step + 1} ========")
+            debug(printDebug, "[TEST] RESETTING LFSR")
 
-          for (i <- 0 until numIncrements) {
+            dut.io.resetLfsr.poke(true.B)
+            dut.clock.step()
+            dut.io.resetLfsr.poke(false.B)
 
-            println(s"[TEST] ======== Step ${i + 1} ========")
+            refLfsr.reset()
 
-            if (i == (whenToReset - 1)) {
-              println(s"[TEST] RESETTING LFSR")
-              c.io.resetLfsr.poke(true.B)
-              c.clock.step()
-              c.io.resetLfsr.poke(false.B)
+            debug(printDebug, "[TEST] States should be the same after reset")
+            debug(printDebug, s"[TEST] Reference state after reset: 0x${refLfsr.getStateHex()}")
+            debug(printDebug, s"[TEST] DUT state after reset:       0x${dut.io.state.peek().litValue.toString(16).toUpperCase}")
 
-              refLfsr.reset()
-              println(s"[TEST] States should be the same after reset")
-              val dutStateHexStr =
-                c.io.state.peek().litValue.toString(16).toUpperCase
-              println(
-                s"[TEST] Reference state after reset: 0x${refLfsr.getStateHex()}"
-              )
-              println(
-                s"[TEST] DUT state after reset:       0x${dutStateHexStr}"
-              )
-
-              assert(
-                refLfsr.getStateHex() == dutStateHexStr,
-                "DUT state doesn't match the reference state"
-              )
-            } else {
-              var refBitStr = ""
-              for (i <- 0 until dataWidth) {
-                val msb = refLfsr.increment()
-                refBitStr = msb.toString + refBitStr
-              }
-
-              val dutOutputBitStr = c.io.lfsrOutput
-                .peek()
-                .litValue
-                .toString(2)
-                .reverse
-                .padTo(dataWidth, '0')
-                .reverse
-              println(s"[TEST] Reference output bit string: ${refBitStr}")
-              println(s"[TEST] DUT output bit string:       ${dutOutputBitStr}")
-
-              assert(
-                refBitStr == dutOutputBitStr,
-                "DUT output doesn't match the reference output"
-              )
-
-              c.io.increment.poke(true.B)
-              c.clock.step()
-              c.io.increment.poke(false.B)
-
-              val dutStateHexStr =
-                c.io.state.peek().litValue.toString(16).toUpperCase
-              println(
-                s"[TEST] Reference state after incrementing: 0x${refLfsr.getStateHex()}"
-              )
-              println(
-                s"[TEST] DUT state after incrementing:       0x${dutStateHexStr}"
-              )
-
-              assert(
-                refLfsr.getStateHex() == dutStateHexStr,
-                "DUT state doesn't match the reference state"
-              )
-            }
+            dut.io.state.expect(refLfsr.getState().U, "DUT state doesn't match the reference state")
+          } else {
+            checkOutputAndIncrement(dut, refLfsr, step)
           }
-          println("[TEST] Success")
+        }
+
+        debug(printDebug, "[TEST] Success")
       }
     }
   }
