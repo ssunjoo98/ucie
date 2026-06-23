8383fa64efca61e3ece20204df7a5054bcf9ffb6
Roshan Parekh <35009954+roshanparekh@users.noreply.github.com>
2026-06-22 19:47:56 -0700

    update ser21 model

--- scala/resources/vsrc/Ser21.v
+++ scala/resources/vsrc/Ser21.v
@@ -1,38 +1,28 @@
-module Ser21 (
-  input  wire clk,    
-  input  wire reset,  
-  input  wire d0,     
-  input  wire d1,     
-  output wire out     
+module ser21 (
+  input  wire clk,
+  input  wire d0,
+  input  wire d1,
+  output wire out
 );
 
-  reg d0_hold;
-  reg d1_int;
-  reg d1_hold;
+  reg d0_hold = 1'b0;
+  reg d1_int = 1'b0;
+  reg d1_hold = 1'b0;
 
+  /* verilator lint_off LATCH */
   always @(*) begin
-    if (reset) begin
-      d0_hold = 1'b0;
-    end else if (!clk) begin
+    if (!clk) begin
       d0_hold = d0;
-    end
-  end
-
-  always @(*) begin
-    if (reset) begin
-      d1_int = 1'b0;
-    end else if (!clk) begin
       d1_int = d1;
     end
   end
 
   always @(*) begin
-    if (reset) begin
-      d1_hold = 1'b0;
-    end else if (clk) begin
+    if (clk) begin
       d1_hold = d1_int;
     end
   end
+  /* verilator lint_on LATCH */
 
-  assign out = clk ? d1_hold : d0_hold;
+  assign out = clk ? d0_hold : d1_hold;
 endmodule
